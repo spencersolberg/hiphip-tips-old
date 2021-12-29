55f73731a5cf6575c12fd3d6563d1ca15c8cbd95
Spencer <you@example.com>
2021-12-28 20:01:06 -0600

    new font ig

--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
 export { serve } from "https://deno.land/std@0.114.0/http/server.ts";
 export { h, ssr, tw } from "https://crux.land/nanossr@0.0.1";
+export * as base64 from "https://denopkg.com/chiefbiiko/base64/mod.ts";
+
